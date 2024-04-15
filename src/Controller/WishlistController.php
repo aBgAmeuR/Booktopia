c5b5ba7b7321f97eca2428b6d29f768c6f38ddef
aBgAmeuR <a.josset@yahoo.com>
2024-04-15 10:00:44 +0200

    feat: view wishlist (part 2)

--- src/Controller/WishlistController.php
+++ src/Controller/WishlistController.php
@@ -7,56 +7,61 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Catalogue\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\DTO\SearchDto;
 use App\Entity\Utilisateur\Utilisateur;
-
 
 class WishlistController extends AbstractController
 {
-    #[Route('/wishlist/add/{id}', name: 'wishlist_add', methods: ['POST'])]
-    public function add(Article $article, EntityManagerInterface $entityManager): Response
+    #[Route('/wishlist/toggle', name: 'app_wishlist_toggle', methods: ['POST'])]
+    public function toggle(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $articleId = $request->get('articleId');
+        $isInWishlist = $request->get('isInWishlist');
+
+        $article = $entityManager->getRepository(Article::class)->find($articleId);
+        $user = $this->getUser();
+
+        if ($isInWishlist) {
+            $user->getWishlist()->removeArticle($article);
+        } else {
+            $user->getWishlist()->addArticle($article);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_product', ['id' => $article->getId()]);
+    }
+
+    #[Route('/wishlist/delete', name: 'app_wishlist_delete')]
+    public function delete(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $articleId = $request->get('articleId');
+        $article = $entityManager->getRepository(Article::class)->find($articleId);
+        $user = $this->getUser();
+
+        $user->getWishlist()->removeArticle($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_wishlist');
+    }
+
+    #[Route('/favorites', name: 'app_wishlist')]
+    public function index(): Response
+    {
+        $searchDto ??= new SearchDto();
         /** @var Utilisateur $user */
         $user = $this->getUser();
+
         if (!$user) {
-            // Rediriger l'utilisateur vers la page de connexion ou donner une erreur
             return $this->redirectToRoute('app_login');
         }
 
-        $wishlist = $user->getWishlist();
-
-        if (!$wishlist) {
-            // Créer la wishlist si elle n'existe pas
-            $wishlist = new Wishlist();
-            $wishlist->setUtilisateur($user);
-            $user->setWishlist($wishlist);
-            $entityManager->persist($wishlist);
-        }
-
-        if (!$wishlist->getArticles()->contains($article)) {
-            $wishlist->addArticle($article);
-            $entityManager->flush();
-
-            return $this->json(['status' => 'added', 'message' => 'Article added to wishlist']);
-        }
-
-        return $this->json(['status' => 'exists', 'message' => 'Article already in wishlist']);
-    }
-
-    #[Route('/wishlist/remove/{id}', name: 'wishlist_remove', methods: ['POST'])]
-    public function remove(Article $article, EntityManagerInterface $entityManager): Response
-    {
-        /** @var Utilisateur $user */
-        $user = $this->getUser();
-        $wishlist = $user->getWishlist();
-
-        if ($wishlist->getArticles()->contains($article)) {
-            $wishlist->removeArticle($article);
-            $entityManager->flush();
-
-            return $this->json(['status' => 'removed', 'message' => 'Article removed from wishlist']);
-        }
-
-        return $this->json(['status' => 'not_exists', 'message' => 'Article not in wishlist']);
+        $articles = $user->getWishlist()->getArticles();
+        return $this->render('wishlist/index.html.twig', [
+            'articles' => $articles,
+            'searchDto' => $searchDto,
+        ]);
     }
 
 
